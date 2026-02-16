e6d32c929899742c5d74db38f587af56bd8623e9
Ian <idmooney@mavs.colradomesa.edu>
2026-02-15 18:29:32 -0700

    Oops I forgot to push the working set up

--- ALDTPA/Bluetooth/phyphox.cpp
+++ ALDTPA/Bluetooth/phyphox.cpp
@@ -1,45 +1,31 @@
-#include <iostream>
-#include <string>
-#include <stdexcept>
 #include <curl/curl.h>
+#include <nlohmann/json.hpp>
+#include <string>
+#include <iostream>
 
-// Callback to handle incoming data from curl
-size_t WriteCallback(void* contents, size_t size, size_t nmemb, void* userp) {
-    ((std::string*)userp)->append((char*)contents, size * nmemb);
-    return size * nmemb;
+using json = nlohmann::json;
+
+static size_t WriteCallback(void* contents, size_t size, size_t nmemb, std::string* output) {
+    size_t totalSize = size * nmemb;
+    output->append((char*)contents, totalSize);
+    return totalSize;
 }
 
 int main() {
-    // Replace with your Phyphox live data URL
-    std::string url = "http://http://192.168.1.64//get?accX&accY&accZ";
     CURL* curl;
     CURLcode res;
     std::string readBuffer;
-
-    curl_global_init(CURL_GLOBAL_DEFAULT);
+    
+    std::cout << "Testing!" << std::endl;
     curl = curl_easy_init();
-
-    if (curl) {
-        curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
+    if(curl) {
+        curl_easy_setopt(curl, CURLOPT_URL, "http://10.4.46.233/get?buffer=accX&buffer=accY&buffer=accZ&buffer=gyrX&buffer=gyrY&buffer=gyrZ");
         curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, WriteCallback);
         curl_easy_setopt(curl, CURLOPT_WRITEDATA, &readBuffer);
 
-        // Optional: set timeout
-        curl_easy_setopt(curl, CURLOPT_TIMEOUT, 5L);
-
-        // Perform the request
         res = curl_easy_perform(curl);
-
-        if (res != CURLE_OK) {
-            std::cerr << "curl_easy_perform() failed: " 
-                      << curl_easy_strerror(res) << std::endl;
-        } else {
-            std::cout << "Raw Data:\n" << readBuffer << std::endl;
-        }
-
         curl_easy_cleanup(curl);
     }
 
-    curl_global_cleanup();
-    return 0;
+    std::cout << readBuffer << std::endl;
 }
