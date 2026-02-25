34209a168c44c9f7a21a902912c0b4725408a2a2
Ian <idmooney@mavs.colradomesa.edu>
2026-02-24 19:38:20 -0700

    got my own code now!

--- ALDTPA/Bluetooth/phyphox.cpp
+++ ALDTPA/Bluetooth/phyphox.cpp
@@ -1,15 +1,16 @@
-#include "Phyphox.h"
+#include "./Phyphox.h"
 #include <iostream>
 
 int main() {
     std::string ip;
+    std::cout << "[NOTICE]: Pleas make sure you are connected to the same wifi for your phone and PC" << std::endl;
     std::cout << "Enter Phyphox IP (example 192.168.0.5:8080): ";
     std::cin >> ip;
 
-    PhyphoxPoller poller(ip);
+    Phyphox poller(ip);
 
     std::cout << "Starting poll...\n";
-    poller.pollLoop(50);
-
+    poller.Phyphox_loop(50);
+    
     return 0;
 } 
--- ALDTPA/Bluetooth/phyphox.h
+++ ALDTPA/Bluetooth/phyphox.h
@@ -1,113 +1,97 @@
 #pragma once
-#include <curl/curl.h>
-#include <nlohmann/json.hpp>
+
 #include <string>
 #include <iostream>
 #include <thread>
 #include <chrono>
 
-using json = nlohmann::json;
+#include <curl/curl.h>
+#include <nlohmann/json.hpp>
 
-class PhyphoxPoller {
+using json = nlohmann::json;
+class Phyphox{
 public:
-    PhyphoxPoller(const std::string& ip)
-        : baseURL("http://" + ip + "/get"),
-          lastAccTime(0.0),
-          lastGyroTime(0.0)
+    Phyphox(std::string ip) : BaseURL("http://" + ip + "/get"), prevAcc(0.0), prevGyro(0.0), pause(0)
     {
         curl = curl_easy_init();
     }
 
-    ~PhyphoxPoller() {
-        if (curl)
+    ~Phyphox()
+    {
+        std::cout << "Deconstructor called, cleaning curl..." << std::endl;
+        if (curl){
             curl_easy_cleanup(curl);
+        }
     }
 
-    void pollLoop(int intervalMs = 50) {
-        while (true) {
-            std::string response;
-            std::string url = buildURL();
-
-            curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
-            curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, writeCallback);
-            curl_easy_setopt(curl, CURLOPT_WRITEDATA, &response);
-
+    int Phyphox_loop(int wait)
+    {
+        do
+        {
+            std::string recieve;
+            std::string url = makeURL();
+            
+            curl_easy_setopt(curl, CURLOPT_URL, url.c_str()); // confused why things are broke
+            curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, WriteCallback);
+            curl_easy_setopt(curl, CURLOPT_WRITEDATA, &recieve);
+            
             CURLcode res = curl_easy_perform(curl);
-            if (res == CURLE_OK) {
-                parseAndPrint(response);
-            } else {
-                std::cerr << "Curl error\n";
+            if (res == CURLE_OK){
+                JSON(recieve);
             }
-
-            std::this_thread::sleep_for(std::chrono::milliseconds(intervalMs));
-        }
-    } 
+            else 
+            {
+                pause = 0;
+                if (res != CURLE_OK) 
+                {
+                    wait++;
+                    std::cout << "Currl error, waiting: " << 5 - wait << std::endl;
+                }
+                if (wait == 5)
+                {
+                    std::cout << "[ERR]: Lost connection to phone, please check connection and reset phone graphs..." << std::endl;
+                    return -1; // temp for now, hope to reset makeURL to base state and allow game to continue after fixing connection
+                }
+            }
+        std::this_thread::sleep_for(std::chrono::milliseconds(wait));
+        }while(true);
+    }
 
 private:
-    CURL* curl;
-    std::string baseURL;
-    double lastAccTime;
-    double lastGyroTime;
+    CURL *curl;
+    std::string BaseURL;
+    float prevAcc;
+    float prevGyro;
 
-    static size_t writeCallback(void* contents, size_t size, size_t nmemb, void* userp) {
+    int pause;
+
+    std::string makeURL()
+    {
+        return BaseURL + "?accX=" + std::to_string(prevAcc) + "|acc_time" +
+                         "&accY=" + std::to_string(prevAcc) + "|acc_time" +
+                         "&accZ=" + std::to_string(prevAcc) + "|acc_time" +
+                         "&acc_time=" + std::to_string(prevAcc) +
+                         "&gyroX=" + std::to_string(prevGyro) + "|gyro_time" +
+                         "&gyroY=" + std::to_string(prevGyro) + "|gyro_time" +
+                         "&gyroZ=" + std::to_string(prevGyro) + "|gyro_time" +
+                         "&gyro_time=" + std::to_string(prevGyro);
+    }
+
+    static size_t WriteCallback(void* contents, size_t size, size_t nmemb, void* userp) {
         size_t totalSize = size * nmemb;
         std::string* str = static_cast<std::string*>(userp);
         str->append(static_cast<char*>(contents), totalSize);
         return totalSize;
     }
-
-    std::string buildURL() {
-        return baseURL +
-            "?accX=" + std::to_string(lastAccTime) + "|acc_time" +
-            "&accY=" + std::to_string(lastAccTime) + "|acc_time" +
-            "&accZ=" + std::to_string(lastAccTime) + "|acc_time" +
-            "&acc_time=" + std::to_string(lastAccTime) +
-            "&gyroX=" + std::to_string(lastGyroTime) + "|gyro_time" +
-            "&gyroY=" + std::to_string(lastGyroTime) + "|gyro_time" +
-            "&gyroZ=" + std::to_string(lastGyroTime) + "|gyro_time" +
-            "&gyro_time=" + std::to_string(lastGyroTime);
-    }
-
-    void parseAndPrint(const std::string& response) {
-        try {
-            json j = json::parse(response);
-
-            auto& buffer = j["buffer"];
-
-            if (buffer.contains("acc_time")) {
-                auto accTimes = buffer["acc_time"]["buffer"];
-                auto accX = buffer["accX"]["buffer"];
-                auto accY = buffer["accY"]["buffer"];
-                auto accZ = buffer["accZ"]["buffer"];
-
-                if (!accTimes.empty()) {
-                    lastAccTime = accTimes.back();
-
-                    std::cout << "ACC  | "
-                              << accX.back() << ", "
-                              << accY.back() << ", "
-                              << accZ.back() << std::endl;
-                }
-            }
-
-            if (buffer.contains("gyro_time")) {
-                auto gyroTimes = buffer["gyro_time"]["buffer"];
-                auto gyroX = buffer["gyroX"]["buffer"];
-                auto gyroY = buffer["gyroY"]["buffer"];
-                auto gyroZ = buffer["gyroZ"]["buffer"];
-
-                if (!gyroTimes.empty()) {
-                    lastGyroTime = gyroTimes.back();
-
-                    std::cout << "GYRO | "
-                              << gyroX.back() << ", "
-                              << gyroY.back() << ", "
-                              << gyroZ.back() << std::endl;
-                }
-            }
+    void JSON(std::string response)
+    {
+        json j = json::parse(response);
+        try{
+            std::cout << j << std::endl;
         }
-        catch (...) {
-            std::cerr << "JSON parse error\n";
+        catch(...){
+            std::cout << "[WARN]: JSON could not parse most recent Phyphox data..." << std::endl;
         }
+        
     }
 };
